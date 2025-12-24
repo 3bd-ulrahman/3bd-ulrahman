5bae1551ce04cc16188ada01839b788e529739db
3bd-ulrahman <abdulrahman198276301@gmail.com>
2025-12-24 11:20:03 +0200

    update base bath

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/3bd-ulrahman/",
   plugins: [vue(), tailwindcss()],
   build: {
     outDir: "docs"
